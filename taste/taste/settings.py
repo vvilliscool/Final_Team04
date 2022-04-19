8be46a6f0cb2ab5bb48de5e9321f16fe7d731085
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-04-19 13:43:48 +0900

    Update django
    index.html

--- taste/taste/settings.py
+++ taste/taste/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'taste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,10 +75,11 @@ WSGI_APPLICATION = 'taste.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
