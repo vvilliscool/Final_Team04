6af0c0a425953195c6c803276683cd411a37f817
Jeon Dong Jun <513ehdwnsl@gmail.com>
2022-05-06 09:46:01 +0900

    220506 장고 프로필 수정 기능 추가

--- meokjalal/taste/review/models.py
+++ meokjalal/taste/review/models.py
@@ -1,5 +1,3 @@
-# from msilib.schema import RadioButton
-# from tkinter import Widget
 from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
--- meokjalal/taste/taste/settings.py
+++ meokjalal/taste/taste/settings.py
@@ -89,22 +89,14 @@ WSGI_APPLICATION = 'taste.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    # #     # 'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
-    #     'NAME': 'meok4',
-    #     'USER': 'meok4',
-    #     'PASSWORD': '1234',
-    #     'HOST': 'localhost', # 동준 IP입니다. 상시 가동입니다. '211.172.73.14
-    #     'PORT': '3306',
-    # }
     'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'PTJ03',
-    'USER': 'test04',
-    'PASSWORD': '1234',
-    'HOST': 'localhost',
-    'PORT': '3306' 
+        'ENGINE': 'django.db.backends.mysql',
+    #     # 'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        'NAME': 'meok4',
+        'USER': 'meok4',
+        'PASSWORD': '1234',
+        'HOST': 'localhost', # 동준 IP입니다. 상시 가동입니다. '211.172.73.14
+        'PORT': '3306',
     }
 }
 
