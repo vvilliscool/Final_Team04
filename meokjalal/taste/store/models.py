1415d8c57c7dc80215225cec25f060e9467f23fc
codnjs3575 <codnjs3575@naver.com>
2022-05-11 14:56:11 +0900

    update store_models.py

--- meokjalal/taste/store/models.py
+++ meokjalal/taste/store/models.py
@@ -13,11 +13,6 @@ class Store(models.Model):
     lot = models.FloatField()
     s_status = models.TextField()
     modification_time = models.DateTimeField()
-    detail = models.ForeignKey('store.Detail', on_delete=models.CASCADE)
-
-    class Meta:
-        managed = False
-        db_table = 'store_store'
 
     def __str__(self):
         return f'Store (PK: {self.pk})'       
@@ -33,9 +28,6 @@ class Detail(models.Model):
     s_menu = models.TextField(blank=True, null=True)
     s_price = models.TextField(blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'store_detail'
 
     def __str__(self):
         return f'Detail (PK: {self.pk})'
