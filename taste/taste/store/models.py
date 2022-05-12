438775e0e4fdad8f281f048d44feffd8a10de67b
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-05-12 13:13:02 +0900

    Update models.py
    Meta 변경

--- taste/taste/store/models.py
+++ taste/taste/store/models.py
@@ -5,14 +5,17 @@ from django.db import models
 
 class Store(models.Model):
     id = models.BigIntegerField(primary_key=True, null=False)
-    s_name = models.TextField()
+    s_name = models.TextField(null=True)
     s_add = models.TextField(null=True)
-    s_road = models.TextField()
-    s_kind = models.TextField()
-    lat = models.FloatField()
-    lot = models.FloatField()
-    s_status = models.TextField()
-    modification_time = models.DateTimeField()
+    s_road = models.TextField(null=True)
+    s_kind = models.TextField(null=True)
+    lat = models.FloatField(null=True)
+    lot = models.FloatField(null=True)
+    s_status = models.TextField(null=True)
+    modification_time = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'store_store'
 
     def __str__(self):
         return f'Store (PK: {self.pk})'       
@@ -28,6 +31,8 @@ class Detail(models.Model):
     s_menu = models.TextField(blank=True, null=True)
     s_price = models.TextField(blank=True, null=True)
 
+    class Meta:
+        db_table = 'store_detail'
 
     def __str__(self):
         return f'Detail (PK: {self.pk})'
