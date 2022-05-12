339067c6038a023b4204c9be92a522eae8fe9da8
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-05-12 10:02:31 +0900

    Update weather.sql

--- backend/weather.sql
+++ backend/weather.sql
@@ -30,7 +30,7 @@ CREATE TABLE `store_weather` (
   `cloud` text,
   `rain` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
